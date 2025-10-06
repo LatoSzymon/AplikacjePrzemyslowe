86d05d504adfbaf546192a380f7bfe902596d1f2
Szymon Lato <szymek.lato@gmail.com>
2025-10-06 22:26:54 +0200

    Initializing spring

---
+++ apki_przemys-owe/src/main/java/LatoRok3/apki_przemys/owe/AplikacjePrzemysłoweApplication.java
@@ -0,0 +1,13 @@
+package LatoRok3.apki_przemys.owe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacjePrzemysłoweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikacjePrzemysłoweApplication.class, args);
+	}
+
+}
---
+++ apki_przemys-owe/src/test/java/LatoRok3/apki_przemys/owe/AplikacjePrzemysłoweApplicationTests.java
@@ -0,0 +1,13 @@
+package LatoRok3.apki_przemys.owe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikacjePrzemysłoweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
